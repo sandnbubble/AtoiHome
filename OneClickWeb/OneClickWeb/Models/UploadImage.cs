68384b15af7af08a384f692d897809158773df44
atoi <atoi@nate.com>
2019-01-08 00:55:37 +0900

    OneClickWeb

---
+++ OneClickWeb/OneClickWeb/Models/UploadImage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace OneClickWeb.Models
+{
+    public class UploadImage
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+    public class UploadImageDBContext : DbContext
+    {
+        public DbSet<UploadImage> UploadImages { get; set; }
+    }
+}
